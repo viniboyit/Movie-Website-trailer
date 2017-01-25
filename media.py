6fb79c4b4db006cc5c6745b1a0771ff1e061c5ec
Vince Kneub <mo.kneub@gmail.com>
2017-01-25 14:29:48 +0100

    third attempt

--- media.py
+++ media.py
@@ -1,15 +1,16 @@
 import webbrowser
 
+
 class Movie:
     """ Create a data structure to store your favorite movies,
     including movie title, storyline, box art URL and a YouTube link to the
     movie trailer.
     """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer):
-        self.title = movie_title # Title of the movie
-        self.storyline = movie_storyline # THe storyline
-        self.poster_image_url = poster_image # The  poster image
-        self.trailer_youtube_url = trailer  #Video trailer
+        self.title = movie_title  # Title of the movie
+        self.storyline = movie_storyline  # THe storyline
+        self.poster_image_url = poster_image  # The  poster image
+        self.trailer_youtube_url = trailer   # Video trailer
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
